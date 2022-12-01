9d9617927fc243cf9ba4aa04842b8e94b4345e0c
fronzen100826 <cmgg919@gmail.com>
2022-12-01 02:29:39 -0500

    commit

--- express_server.js
+++ express_server.js
@@ -4,22 +4,23 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
-function generateRandomString() {}
 
-
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
