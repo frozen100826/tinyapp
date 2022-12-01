f86641b6a1ee306e222e12b7bd92ffb198b33ed8
fronzen100826 <cmgg919@gmail.com>
2022-11-30 22:59:37 -0500

    first

---
+++ express_server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+function generateRandomString() {}
+
+
